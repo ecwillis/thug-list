8c182df418c56b7dac25d29b5bb6df986983af8f
Eric Willis <eric@ecwillis.com>
2015-01-07 15:26:01 -0800

    Swapping swig for jade to work with angular

--- app.js
+++ app.js
@@ -5,14 +5,18 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var routes = require('./routes/index');
+var main = require('./routes/main');
 var users = require('./routes/users');
+
+var swig = require('swig');
 
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+// For ease of use use the swig
+app.engine('html', swig.renderFile);
+app.set('view engine', 'html');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
@@ -22,7 +26,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
+app.use('/', main);
 app.use('/users', users);
 
 // catch 404 and forward to error handler
